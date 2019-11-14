9f4d9767ee076134d8912b15f4e26e6fd00ef573
faahhz <nurulafifah25594@gmail.com>
2019-11-15 05:13:14 +0700

    udh di gabung

---
+++ application/views/userViews/partials/mybag.php
@@ -0,0 +1,107 @@
+<div class="w ui-hide cart-main" id="cart-list" style="display: block;">         <div id="jd-cart">
+            <!-- cart header -->
+            <div class="cart-thead">
+                <div class="column t-checkbox cke-box cked">
+                    <span class="cke" clstag="pageclick|keycount|epi_cart_shoppinglist_selectAll_2016120713|0" id="selectAll"></span><label>Pilih Semua</label>
+                </div>
+                <div class="column t-goods">Barang</div>
+                <div class="column t-item-price">Harga</div>
+                <div class="column t-quantity">Jumlah</div>
+                <div class="column t-subtotal">Subtotal</div>
+                <div class="column t-action"></div>
+            </div>
+            <!-- cart body -->
+            <div class="cart-tbody">
+            
+                <div class="cart-item-list ">
+                    <div class="shop cke-box cked">
+                        <span class="cke" clstag="pageclick|keycount|epi_cart_shoppinglist_shop_selectAll_2016120713|1" id="selectSAll_30714" sellerid="30714"></span>
+                        <a clstag="pageclick|keycount|epi_cart_shoppinglist_shop_view_2016120713|2" class="shop-name" href="//www.jd.id/shop/MINISO-INDONESIA_30714.html" id="sellerId_30714">MINISO INDONESIA</a>
+                        <div class="shop-chat-wrapper" sellerid="30714" style="display: none;">
+                             <a clstag="pageclick|keycount|epi_cart_dogndong|0" class="shop-chat-url" href="//chat.jd.id/index.action?venderId=30714&amp;entry=cart" target="_blank">
+                                 <span class="shop-chat">Live Chat</span>
+                             </a>
+                        </div>
+                        <div class="shop-coupon-area" id="shop-coupon-area-30714" style="display: none;">
+                            <span class="shop-coupon">
+                                <a href="javascript:void(0);">kupon</a><i></i>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="item-list">
+       
+      <div class="item-single item-item  active" id="item_508319154_1_0" sellerid="30714" skuid="508319154" skutype="1" promotionid="0" skuuuid="508319154_1_0">
+        <input type="hidden" id="skuId_508319154" value="508319154" skuwareid="53442418">
+        <div class="item-form">
+            <div class="cell p-checkbox cke-box cked">
+                <span class="cke" id="skuUuId_508319154_1_0"></span>
+                <span class="line-circle"></span>
+            </div>
+            <div class="cell p-goods">
+                <a clstag="pageclick|keycount|epi_cart_shoppinglist_viewWare_2016120713|3" href="//www.jd.id/product/seoName_53442418/508319154.html" target="_blank">
+                    <img onerror="errorImg(this)" src="//img10.jd.id/Indonesia//s80x80_/N4/nHBfsgABKgAAAAwADX03nAAAnE0.jpg" alt="Miniso Official Frog Soap Holder GreenFrog"></a>
+            </div>
+            <div class="cell p-name">
+                <span class="spacer"></span>
+                <a clstag="pageclick|keycount|epi_cart_shoppinglist_viewWare_2016120713|3" href="//www.jd.id/product/seoName_53442418/508319154.html" target="_blank" class="v-middle">  Miniso Official Frog Soap Holder GreenFrog [GreenFrog]</a>
+            </div>
+            <div class="cell p-item-price">
+                <span class="spacer"></span>
+                <span class="v-middle">     <span class="p-price">Rp 23,900</span><br>
+    <span class="p-price p-price-old">Rp 49,900</span><br>
+               </span>
+            </div>
+            <div class="cell p-quantity">
+                <span class="spacer"></span>
+                <div class="quantity-wrap v-middle">
+                    <div class="p-num pw-spinner-container">
+                                        <span clstag="pageclick|keycount|epi_cart_shoppinglist_skuCount_reduce_2016120713|4" id="reduce_508319154_1_0" pwjs="pw-spinner-sub" class="fl reduce disable">-</span>
+                        <input type="text" maxlength="4" id="inputNum_508319154_1_0" stock="61" value="1" pwjs="pw-spinner-show" class="fl">
+                        <input type="hidden" id="hid_508319154_1_0" value="1">
+                        <span clstag="pageclick|keycount|epi_cart_shoppinglist_skuCount_Add_2016120713|4" id="add_508319154_1_0" pwjs="pw-spinner-add" class="fl add">+</span>
+                        <div class="tips-msg" id="tips-msg-508319154"><i></i></div>
+                    </div>
+                </div>
+            </div>
+            <div class="cell p-subtotal">
+                <span class="spacer"></span>
+                <strong class="v-middle">Rp 23,900</strong>
+            </div>
+            <div class="cell p-ops">
+                <a href="javascript:;" class="btn-1" clstag="pageclick|keycount|epi_cart_shoppinglist_love_2016120713|5">Pindahkan ke favorite</a>
+                <a href="javascript:;" class="btn-2" clstag="pageclick|keycount|epi_cart_shoppinglist_delete_2016120713|6">Hapus</a>
+            </div>
+        </div>
+<div class="item-line"></div>
+    </div>
+                       </div>
+                </div>
+            </div>
+            <!-- cart footer -->
+                <div class="cart-tfoot fix-bottom">
+                    <div class="cart-error-msg" style="display: none;">Anda tidak dapat mencampur pilihan produk Anda dengan produk tanpa promo Enterprise, Silahkan ganti produk Anda.</div>
+                    <div class="cart-addredd-promo-tps" style="display: none;">Alamat yang anda pilih tidak sesuai dengan yang sebelumnya, mungkin anda akan tidak mendapatkan promosi atau promosi lainny.</div>
+                    <div class="cart-ft-ctrl">
+                        <span class="cke-box f-ib cked">
+                            <span class="cke" clstag="pageclick|keycount|epi_cart_shoppinglist_selectAll_2016120713|0" id="selectAll_2"></span><span>Pilih Semua</span>
+                        </span>
+                        <a class="btn" id="deleteItem" href="javascript:;" clstag="pageclick|keycount|epi_cart_shoppinglist_delSelect_2016120713|8">Hapus yang ditandai</a>
+                    </div>
+                    <div class="cart-ft-right">
+                        <div class="cart-result">
+                            <p class="price-total">
+                                <em class="f-ib">Subtotal                                    (1&nbsp;pieces)</em>
+                                <strong class="notranslate">Rp 23,900</strong>
+                            </p>
+                            <p class="price-discount">
+                                <em class="f-ib">Anda menghemat</em>
+                                <strong class="notranslate">Rp 26,000</strong>
+                            </p>
+                        </div>
+                        <div class="buy-all" id="popCart">
+                            <a href="javascript:;" class="f-ib" clstag="pageclick|keycount|epi_cart_checkout_2016120713|0">Checkout</a>
+                        </div>
+                    </div>
+                </div>
+        </div>
+</div>
