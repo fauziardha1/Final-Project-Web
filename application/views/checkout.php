b035bcbd1f860c7d62569d1342af4977f00a5bb5
faahhz <nurulafifah25594@gmail.com>
2019-11-25 12:44:58 +0700

    menambahkan hal checkout (fix)

--- application/views/checkout.php
+++ application/views/checkout.php
@@ -1,26 +1,39 @@
   <?php $this->load->view("partials/header.php") ?>
    <?php $this->load->view("partials/navbar.php") ?>
-  <div class="row py-5 p-4 bg-white rounded shadow-sm  ">
-        <div class="col-lg-6 ">
+  <div class="row py-5 p-4 bg-white rounded shadow-sm  d-flex justify-content-center ">
+        <div class=" card " style="width: 50%">
           
-          <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Alamat Penerima</div>
+          <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold ">Alamat Penerima</div>
           
-            
-            <textarea name="" cols="30" rows="2" class="form-control"></textarea>
+            <div class=" d-flex justify-content-center">
+            <textarea name="" cols="23" rows="2" class="form-control" style="width: 95%"></textarea>
+            </div>
 
-            <div class="bg-light rounded-pill px-4 py-3 ">Daftar belanja
-              <div class=" p-4 bg-white rounded shadow-sm ">
+            <div class="bg-light rounded-pill px-4 py-3  ">Daftar belanja
+              <div class=" p-4 bg-white rounded shadow-sm  ">
                <tr>
-                  
-                  <th scope="row" class="border-0">
-                    <div class="p-2 ">
+                  <div class="d-flex justify-content-between  class="text-muted font-weight-normal font-italic d-block"">
+                  <th scope="row" class="border-0 ">
+                    <div class="p-2  ">
                       <img src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-1_zrifhn.jpg" alt="" width="70" class="img-fluid rounded shadow-sm">
                       <div class="ml-3 d-inline-block align-middle ">
                         <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block align-middle">Timex Unisex Originals</a></h5><span class="text-muted font-weight-normal font-italic d-block">Category: Watches</span>
                       </div>
                     </div>
                   </th>
-                  <td class="border-0 align-middle float-sm-right text-right">$<strong id="productPrice">79.00</strong></td>
+                  <div class="d-flex align-items-center">
+
+                  <td>
+                    <span  class="text-muted font-weight-normal d-block" >x2</span>
+                  </td>
+                  </div>
+                  </div>
+
+
+                   <div class=" d-flex justify-content-end">
+
+                  <td class="border-0 align-middle ">$<strong id="productPrice">79.00</strong></td>
+                    </div>
 
                 </tr>
 
@@ -31,20 +44,15 @@
 
 
 
-    
-
-
-
-
-
-        <div class="col-lg-6">
+        <div class="card " style="width: 23%">
+       
           <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">rincian pembelian</div>
           <div class="p-4">
             <ul class="list-unstyled ">
-              <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Total Belanja </strong><strong>$390.00</strong></li>
-              <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Diskon</strong><strong>$10.00</strong></li>
-              <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Biaya Pengiriman</strong><strong>$0.00</strong></li>
-              <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Total</strong>
+              <li class="d-flex justify-content-between py-1 border-bottom text-muted">Total Belanja <p> $390.00</p></li>
+              <li class="d-flex justify-content-between py-1 border-bottom text-muted">Diskon <p>$10.00</p></li>
+              <li class="d-flex justify-content-between py-1 border-bottom text-muted">Biaya Pengiriman <p>$0.00</p></li>
+              <li class="d-flex justify-content-between py-3 border-bottom text-muted"><strong>Total</strong>
                 <h5 class="font-weight-bold">$400.00</h5>
               </li>
             </ul><a href="#" class="btn btn-dark rounded-pill py-2 btn-block text-uppercase ">beli</a>
